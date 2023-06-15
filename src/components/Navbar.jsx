3025dcad253d211916071a266b86a2c5e8364c4e
jtphan-asu <jtphan@asu.edu>
2023-06-15 12:05:15 -0700

    Add link to home page on Navbar and add empty link for Resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+
+    //Create div container for navigation and link
+    //to resume
+    <div className='nav-container'>
+      <div className="logo">
+        <Link href="/" >
+          Jonathan's Portfolio
+        </Link>
+      </div>
+
+      <div>
+        <a href='' className="cta-btn">Resume</a>
+      </div>
+    </div>
+    
   )
 }
 
