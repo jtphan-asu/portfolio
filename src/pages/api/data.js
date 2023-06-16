6998e35e487401372b8d4ee140694c47777ae2e5
jtphan-asu <jtphan@asu.edu>
2023-06-16 13:18:58 -0700

    Create Projects.jsx with links to GitHub project

---
+++ src/pages/api/data.js
@@ -0,0 +1,47 @@
+
+//Array data structure to store project data
+export const projectArray = [
+    {
+        id: 1,
+        title: 'Portfolio',
+        description: "Portfolio website using Next.js, Tailwind CSS, React.js",
+        link: "https://github.com/jtphan-asu/portfolio",
+    },
+    {
+        id: 2,
+        title: 'Prompto',
+        description: "Create and Share Prompts for Artificial Intelligence Generators",
+        link: "https://github.com/jtphan-asu/prompto",
+    },
+    {
+        id: 3,
+        title: 'Library',
+        description: "Library CRUD application where books can be added using JSON format",
+        link: "https://github.com/jtphan-asu/library_TOP",
+    },
+    {
+        id: 4,
+        title: 'Flask-Web-App',
+        description: "Note Taking Application using Flask and Python Programming language",
+        link: "https://github.com/jtphan-asu/Flask-Web-App",
+    },
+    {
+        id: 5,
+        title: 'Rock, Paper, Scissors!',
+        description: "Rrock, paper, scissors game against the CPU built using JavaScript.",
+        link: "https://github.com/jtphan-asu/rock_paper_scissors_TOP",
+    },
+    {
+        id: 6,
+        title: 'Admin Dashboard',
+        description: "Dashboard application using HTML and CSS",
+        link: "https://github.com/jtphan-asu/admin-dashboard",
+    },
+
+
+]
+
+
+
+    
+
