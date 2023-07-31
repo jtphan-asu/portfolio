7337633d4710015209e6ef6f35db200b914c61c5
Jonathan Phan <jtphan@asu.edu>
2023-07-31 10:44:23 -0700

    Add Homes project to data.js

--- src/pages/api/data.js
+++ src/pages/api/data.js
@@ -15,24 +15,30 @@ export const projectArray = [
     },
     {
         id: 3,
+        title: 'Homes',
+        description: "Home buying application built with Angular Typescript and JSON-server",
+        link: "https://github.com/jtphan-asu/homes",
+    },
+    {
+        id: 4,
         title: 'Library',
         description: "Library CRUD application where books can be added using JSON format",
         link: "https://github.com/jtphan-asu/library_TOP",
     },
     {
-        id: 4,
+        id: 5,
         title: 'Flask-Web-App',
         description: "Note Taking Application using Flask and Python Programming language",
         link: "https://github.com/jtphan-asu/Flask-Web-App",
     },
     {
-        id: 5,
+        id: 6,
         title: 'Rock, Paper, Scissors!',
         description: "Rrock, paper, scissors game against the CPU built using JavaScript.",
         link: "https://github.com/jtphan-asu/rock_paper_scissors_TOP",
     },
     {
-        id: 6,
+        id: 7,
         title: 'Admin Dashboard',
         description: "Dashboard application using HTML and CSS",
         link: "https://github.com/jtphan-asu/admin-dashboard",
